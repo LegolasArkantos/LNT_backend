88e062f0deaa7782ee1df700f3b859d292844b87
LegolasArkantos <unasaamir101@gmail.com>
2024-04-12 23:11:20 +0500

    forgot to change

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
@@ -67,7 +67,7 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
   },
 });
 
