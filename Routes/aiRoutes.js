c5cbc4c5d865e303da5e8c9eb7cc62dc132caff0
LegolasArkantos <unasaamir101@gmail.com>
2024-05-29 00:48:06 +0500

    added new apis

--- Routes/aiRoutes.js
+++ Routes/aiRoutes.js
@@ -7,6 +7,8 @@ const authMiddleware = require('../Middleware/Auth.middleware')
 router.post("/generate-story", aiController.generateStory);
 router.get("/generate-analysis",authMiddleware.authenticateUser, aiController.generateAnalysis);
 router.get("/generate-analysis-student",authMiddleware.authenticateUser, aiController.generateAnalysisStudent);
+router.get("/analytics/:sessionId",authMiddleware.authenticateUser, aiController.graphData);
+router.get("/sessions",authMiddleware.authenticateUser, aiController.getSessionids);
 
 
 module.exports = router;
